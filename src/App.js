c387e36ba2dee141299bf43aba37a9e38506529d
tanyavolyn <tanyavolyn@gmail.com>
2023-05-20 16:16:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const[activity, setActivity] = useState("");
+  const [style, setStyle] = useState("text-activity-one");
+
+  const changeStyle = () => {
+
+  
+    setStyle("text-activity-two");
+  };
+  
+
+   useEffect(()=>{
+    getActivity();
+   }, [])
+
+   const getActivity = async() => {
+    const textActivity = await fetch (`http://www.boredapi.com/api/activity/`);
+    const data = await textActivity.json();
+    setActivity(data.activity)
+   }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='text'>
+      <h1 class="title">
+    <span class="title-word title-word-1">What </span>
+    <span class="title-word title-word-2">can </span>
+    <span class="title-word title-word-3">I </span>
+    <span class="title-word title-word-4">do...</span>
+  </h1>
+
+  <div className='block'>
+    
+      <p className={style}>{activity}</p>
+     
+      <div className='btn'>
+      <button  onClick={() => {getActivity(); changeStyle();}}>SHOW</button>
+      </div>
+      </div>
+      </div>
     </div>
   );
 }
